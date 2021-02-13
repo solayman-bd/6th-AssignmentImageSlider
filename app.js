34832ca9d333f08c6dc003d57e1c46e234a5b26d
Md. Solayman <mdsolaymannuhin@gmail.com>
2021-02-14 01:52:29 +0600

    4thIssueSolved

--- app.js
+++ app.js
@@ -124,3 +124,13 @@ searchBtn.addEventListener("click", function () {
 sliderBtn.addEventListener("click", function () {
   createSlider();
 });
+
+search.addEventListener("keyup", function (event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    searchBtn.click();
+  }
+});
